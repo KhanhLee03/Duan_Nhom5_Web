deb5a46b262c4bdb48c1490f4eafeab6483d5528
KhanhLee03 <lekhanhbg2k3@gmail.com>
2024-10-30 22:13:12 +0700

    Delete code directory

--- code/chitiettintuc.php
+++
@@ -1,5 +0,0 @@
-<?php include"headernguoidung.php";?>
-<?php include"chitiettintuc1.php";?>
-<?php include"chitiettintuc2.php";?>
-<?php include"chitiettintuc3.php";?>
-<?php include"footernguoidung.php";?>
