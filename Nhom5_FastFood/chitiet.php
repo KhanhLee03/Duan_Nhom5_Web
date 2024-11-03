6167d10e8365a68f76435251b7040e24cf0f93a7
KhanhLee03 <lekhanhbg2k3@gmail.com>
2024-11-03 20:50:34 +0700

    Delete Nhom5_FastFood directory

--- Nhom5_FastFood/chitiet.php
+++
@@ -1,4 +0,0 @@
-<?php include "headernguoidung.php";?>
-<?php include "chitiet1.php";?>
-<?php include "chitiet2.php";?>
-<?php include "footernguoidung.php";?>
--- Nhom5_FastFood/index.php
+++
@@ -1,5 +0,0 @@
-<?php include"headernguoidung.php";?>
-<?php include"main1.php";?>
-<?php include"main2.php";?>
-<?php include"main3.php";?>
-<?php include"footernguoidung.php";?>
